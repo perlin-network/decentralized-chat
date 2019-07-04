ae5aabd4cdaf5ad07ba7feffd47ea38bd9306b7e
Kenta Iwasaki <kenta@perlin.net>
2019-07-04 16:23:36 +0900

    Updates

---
+++ precache-manifest.cf8b18c21843f50f8192d049b9d7369a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edfef79ace367120d65b01d766fb0e3f",
+    "url": "/decentralized-chat/index.html"
+  },
+  {
+    "revision": "19e7b23b80858cb65885",
+    "url": "/decentralized-chat/static/css/main.44e2af58.chunk.css"
+  },
+  {
+    "revision": "7e9c83d8e13bb18af3f2",
+    "url": "/decentralized-chat/static/js/2.46ff530b.chunk.js"
+  },
+  {
+    "revision": "19e7b23b80858cb65885",
+    "url": "/decentralized-chat/static/js/main.8019f87f.chunk.js"
+  },
+  {
+    "revision": "8f0ffcfcbe9e0910c237",
+    "url": "/decentralized-chat/static/js/runtime~main.f73111cf.js"
+  }
+]);
